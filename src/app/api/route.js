f45442942df335ce6feeee59f1a163049805bb73
sh20raj <shaswatraj3@gmail.com>
2024-04-02 21:39:35 +0530

    Initial commit from Create Next App

---
+++ src/app/api/route.js
@@ -0,0 +1,18 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+// Access your API key (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI("AIzaSyCXTVHIyCWBioY2mq6NtfPMcJqdkjOFleU");
+
+async function run() {
+  // For text-only input, use the gemini-pro model
+  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+
+  const prompt = "Write a story about a magic backpack."
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+}
+
+run();
